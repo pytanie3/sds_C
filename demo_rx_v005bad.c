dcd988f273c753b55b370d1ff1765e1c429a38bd
Piotr Kasprzyk <ciri@ciri.pl>
2018-11-30 16:07:28 +0100

    Dodaj nagłówek

--- demo_rx_v005bad.c
+++ demo_rx_v005bad.c
@@ -120,6 +120,7 @@ int main(void)
         tm_ptr->tm_mday
         );
     out_file = fopen(out_text, "w");
+    sprintf(out_text, "Time;PM 2.5 [ug/m^3];PM 10 [ug/m^3]\n");
     while(1)
     {
         if(char_cnt > 0)
