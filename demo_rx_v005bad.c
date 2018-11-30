80c2b39437d957a55f57eb3140607f5f71b5223a
Piotr Kasprzyk <ciri@ciri.pl>
2018-11-30 14:13:24 +0100

    Allow for diagnostics

--- demo_rx_v005bad.c
+++ demo_rx_v005bad.c
@@ -22,6 +22,9 @@ compile with the command: gcc demo_rx.c rs232.c -Wall -Wextra -o2 -o test_rx
 // #include "c:\\i\Python\My.python\RS-232\rs232.h"
 #include "rs232.h"
 
+#define VERBOSE_CHARS 1
+#define VERBOSE_SUMMARY 1
+
   int RS232_PollComport_full(int cport, unsigned char buf, int nbytes)
   {
     n=1
