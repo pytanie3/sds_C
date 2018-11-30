e7f1cf055daa10941996b2fa24b1e28a437f14f9
Piotr Kasprzyk <ciri@ciri.pl>
2018-11-30 14:23:10 +0100

    Clean main

--- demo_rx_v005bad.c
+++ demo_rx_v005bad.c
@@ -67,63 +67,80 @@ int RS232_PollComport_full(int cport_nr, unsigned char * buf, int nbytes)
     return already_read;
 }
 
-int main()
+int main(void)
 {
-  int byte = 0, lastbyte = 0,
-      PM25_High_byte,PM25_Low_byte,
-      PM10ugpm3=0, PM10_High_byte,PM10_Low_byte, 
-      i=0, j0=0, j1=0,j2=0, n=0, 
-      k_while=0,
-      cport_nr=7,   /*    bylo cport_nr=3     /dev/ttyS0 (COM1 on windows) */
-      bdrate=9600;  /* 9600 baud */
-  float PM25ugpm3=0;
-  unsigned char buf0[1];
-  unsigned char buf1[1];
-  unsigned char buf2[1];
-  unsigned char buf3[8];
-  unsigned char packet[8];
-  unsigned char ind = 0;
-  unsigned char state = 0;      // 0 - oczekujemy na znak 0xAA
-                                // 1 - oczekujemy na znak 0xC0
-                                // 2 - oczekujemy na 8 pozosta³ych bajtów 
-  char mode[]={'8','N','1',0};
-  
-  j0=RS232_PollComport_full(cport_nr, buf0, 1))
-  printf ("Start przed if floats: %4.2f %ld %+.0e  \n", 3.1416, k_while, 3.1416);
-  if(RS232_OpenComport(cport_nr, bdrate, mode))
-  {
-    printf("Can not open comport\n");
-    return(0);
-  }
-  int nj = 10000;
-  while(1)
-  { 
-    j0 = j1;
-    j0 = RS232_PollComport(cport_nr, buf0, 1);
-    j1 = RS232_PollComport(cport_nr, buf1, 1); //RS232_PollComport(cport_nr, buf2, 1);
-    printf("odebrano pakiet\n");
-    printf ("po while(1): j0_prc.ld, j1_prc.ld, buf1[0]_prc.l : %ld %ld %ld \n", j0, j1, buf1[0]); // %4.2f  float
-    if ((buf0[0] == 0xAA)&(buf1[0] == 0xC0)){                                       //if (buf2[0] == 0xAA) {}
-        j2= RS232_PollComport(cport_nr, buf2, 8);
-        printf ("po (buf0[0]==(0xAA))&(buf1[0]==0xC0)");
-        printf ("prc.ld. buf0[0],buf1[0]= %ld; %ld; \n",buf0[0],buf1[0]);
-        printf ("prc.X.  buf0[0],buf1[0]= %X; %X;   \n",buf0[0],buf1[0]);
-        printf ("prc.i.  buf0[0],buf1[0]= %i; %i;   \n",buf0[0],buf1[0]);
-        printf ("prc.ld. buf2[0;1;2;3;4;]= %ld; %ld; %ld; %ld; %ld; \n",buf2[0],buf2[1],buf2[2],buf2[3],buf2[4]);
-        printf ("prc.X.  buf2[0;1;2;3;4;]= %X; %X; %X; %X; %X;      \n",buf2[0],buf2[1],buf2[2],buf2[3],buf2[4]);
-        printf ("prc.d.  buf2[0;1;2;3;4;]= %d; %d; %d; %d; %d;      \n",buf2[0],buf2[1],buf2[2],buf2[3],buf2[4]);
-        printf ("prc.i.  buf2[0;1;2;3;4;]= %i; %i; %i; %i; %i;      \n",buf2[0],buf2[1],buf2[2],buf2[3],buf2[4]);
-        state = 0;
-        PM25_High_byte=buf3[1];
-        PM25_Low_byte=buf3[0];
-        PM25ugpm3 = (float)((((float)PM25_High_byte *256) + (float)PM25_Low_byte)/10);
-        printf (" PM25ugpm3= %4.2f \n",PM25ugpm3);
-        printf (" nj %i \n",nj) ;
-        }
-    if (--nj<0){break;}
+    int byte = 0,
+    lastbyte = 0,
+    PM25_High_byte,
+    PM25_Low_byte,
+    PM10ugpm3=0,
+    PM10_High_byte,
+    PM10_Low_byte,
+    i = 0,
+    j0 = 0,
+    j1 = 0,
+    j2 = 0,
+    n = 0,
+    k_while = 0,
+    cport_nr = 7,   /*    bylo cport_nr = 3     /dev/ttyS0 (COM1 on windows) */
+    bdrate = 9600;  /* 9600 baud */
+    float PM25ugpm3 = 0;
+    unsigned char buf0[1];
+    unsigned char buf1[1];
+    unsigned char buf2[1];
+    unsigned char buf3[8];
+    unsigned char packet[8];
+    unsigned char ind = 0;
+    unsigned char state = 0;      // 0 - oczekujemy na znak 0xAA
+    // 1 - oczekujemy na znak 0xC0
+    // 2 - oczekujemy na 8 pozosta³ych bajtów
+    char mode[] = {'8', 'N', '1', 0};
+
+    j0 = RS232_PollComport_full(cport_nr, buf0, 1);
+    printf ("Start przed if floats: %4.2f %ld %+.0e  \n", 3.1416, k_while, 3.1416);
+    if(RS232_OpenComport(cport_nr, bdrate, mode))
+    {
+        printf("Can not open comport\n");
+        return 0;
     }
-  return(0);
-} //main      
+    int nj = 10000;
+    while(1)
+    {
+        j0 = RS232_PollComport_full(cport_nr, buf0, 1);
+        j1 = RS232_PollComport_full(cport_nr, buf1, 1);
+        printf("odebrano pakiet\n");
+        printf ("po while(1): j0_prc.ld, j1_prc.ld, buf1[0]_prc.l : %ld %ld %ld \n", j0, j1, buf1[0]); // %4.2f  float
+        if ((buf0[0] == 0xAA)&(buf1[0] == 0xC0))
+        {                                       //if (buf2[0] == 0xAA)
+            j2 = RS232_PollComport_full(cport_nr, buf2, 8);
+            printf ("po (buf0[0]==(0xAA))&(buf1[0]==0xC0)");
+            printf ("prc.ld. buf0[0],buf1[0]= %ld; %ld; \n", buf0[0], buf1[0]);
+            printf ("prc.X.  buf0[0],buf1[0]= %X; %X;   \n", buf0[0], buf1[0]);
+            printf ("prc.i.  buf0[0],buf1[0]= %i; %i;   \n", buf0[0], buf1[0]);
+            printf ("prc.ld. buf2[0;1;2;3;4;]= %ld; %ld; %ld; %ld; %ld; \n", buf2[0], buf2[1], buf2[2], buf2[3], buf2[4]);
+            printf ("prc.X.  buf2[0;1;2;3;4;]= %X; %X; %X; %X; %X;      \n", buf2[0], buf2[1], buf2[2], buf2[3], buf2[4]);
+            printf ("prc.d.  buf2[0;1;2;3;4;]= %d; %d; %d; %d; %d;      \n", buf2[0], buf2[1], buf2[2], buf2[3], buf2[4]);
+            printf ("prc.i.  buf2[0;1;2;3;4;]= %i; %i; %i; %i; %i;      \n", buf2[0], buf2[1], buf2[2], buf2[3], buf2[4]);
+            state = 0;
+            PM25_High_byte = buf3[1];
+            PM25_Low_byte = buf3[0];
+            PM25ugpm3 = (
+                (
+                    PM25_High_byte * 256 + PM25_Low_byte
+                )
+                /
+                10.0);
+            printf (" PM25ugpm3= %4.2f\n", PM25ugpm3);
+            printf (" nj %i \n",nj) ;
+        }
+        if (--nj < 0)
+        {
+            break;
+        }
+    }
+    return 0;
+} //main
+
 /*
 # 00 01 02 03 04 05 06 07 08 09
 # AA C0 72 00 90 00 62 F0 54 AB
@@ -134,7 +151,7 @@ int main()
 # 4 DATA 3 PM10 Low byte
 # 5 DATA 4 PM10 High byte
 #PM2.5 value: PM25ugpm3 = ((PM25_High_byte *256) + PM25_low_byte)/10
-PM25_Low_byte = 72 
+PM25_Low_byte = 72
 PM25_High_byte = 00
 struct.unpack(72)
 bytes([00])
